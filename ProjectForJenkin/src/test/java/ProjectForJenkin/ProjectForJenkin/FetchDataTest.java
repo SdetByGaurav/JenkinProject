6fd05ade84f3cbcc48b985edc9ee2ddfed9b021a
Gaurav Rajendra Gaud <gaurav17.gaud@gmail.com>
2019-10-06 12:57:47 +0530

    Project for Jenkins

---
+++ ProjectForJenkin/src/test/java/ProjectForJenkin/ProjectForJenkin/FetchDataTest.java
@@ -0,0 +1,22 @@
+package ProjectForJenkin.ProjectForJenkin;
+
+import static io.restassured.RestAssured.*;
+
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
+public class FetchDataTest {
+
+	@Test
+	public void fetchResouces()
+	{
+		Response resp = given()
+				.param("userId", "2")
+//				.param("id" , "13")
+				.pathParam("key", "posts")
+.get("https://jsonplaceholder.typicode.com/{key}");
+		
+		resp.then().log().all();
+	}
+}
